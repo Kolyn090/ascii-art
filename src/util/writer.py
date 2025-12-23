2fa857c8d95b5c30d79bd0a76b0289eb77d5dc51
Kolyn090 <kolynlin@protonmail.com>
2025-12-23 13:54:58 -0500

    'Big update': center all char templates horizontally in the default writer

--- src/util/writer.py
+++ src/util/writer.py
@@ -8,33 +8,7 @@ from concurrent.futures import ThreadPoolExecutor
 
 from slicer import Cell
 from static import to_binary_strong
-
-class CharTemplate:
-    def __init__(self,
-                 char: str,
-                 image_font: FreeTypeFont,
-                 char_bound: tuple[int, int],
-                 img: np.ndarray,
-                 img_binary: np.ndarray,
-                 img_small: np.ndarray,
-                 img_projection: np.ndarray):
-        self.char = char
-        self.image_font = image_font
-        self.char_bound = char_bound
-        self.img = img
-        self.img_binary = img_binary
-        self.img_small = img_small
-        self.img_projection = img_projection
-
-class PositionalCharTemplate:
-    def __init__(self,
-                 char_template: CharTemplate,
-                 top_left: tuple[int, int]):
-        self.char_template = char_template
-        self.top_left = top_left
-
-    def __str__(self):
-        return f"{{'{self.char_template.char}'{self.top_left}}}"
+from char_template import CharTemplate, PositionalCharTemplate
 
 class Writer:
     def __init__(self,
@@ -250,7 +224,18 @@ class Writer:
                             math.floor(char_bound[1] * self.approx_ratio))
         img = Image.new("RGB", char_bound, "white")
         draw = ImageDraw.Draw(img)
-        draw.text((0, 0), char, font=self.image_font, fill="black")
+
+        # Get text bounding box
+        bbox = draw.textbbox((0, 0), char, font=self.image_font)
+
+        text_w = bbox[2] - bbox[0]
+
+        img_w, img_h = img.size
+
+        # Center horizontally
+        x = (img_w - text_w) // 2
+
+        draw.text((x, 0), char, font=self.image_font, fill="black")
 
         template = np.array(img)
         template_binary = to_binary_strong(template)
