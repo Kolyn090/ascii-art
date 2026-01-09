Fixed padding in gradient writer

src/depth_shade/gradient_writer.py
@@ -36,7 +36,13 @@ class GradientWriter:
             img = self.gradient_imgs[i]
             img = invert_image(img)
             slicer = Slicer()
-            padded_char_bound = self._make_padded_char_bound(self.templates[i].char_bound, template.pad)
+            padded_char_bound = self._make_padded_char_bound(self.templates[i].char_bound,
+                                                             (
+                                                                 template.pad_top,
+                                                                 template.pad_bottom,
+                                                                 template.pad_left,
+                                                                 template.pad_right
+                                                             ))
             cells = slicer.slice(img, padded_char_bound)
             matched_img, p_cts = writer.match_cells(cells)
             self.writer = writer
@@ -47,8 +53,9 @@ class GradientWriter:
         return self.stack_to_img(stacks, w, h), stacks
 
     @staticmethod
-    def _make_padded_char_bound(char_bound: tuple[int, int], pad: tuple[int, int]) -> tuple[int, int]:
-        return char_bound[0] + 2 * pad[0], char_bound[1] + 2 * pad[1]
+    def _make_padded_char_bound(char_bound: tuple[int, int],
+                                pad: tuple[int, int, int, int]) -> tuple[int, int]:
+        return char_bound[0] + pad[2] + pad[3], char_bound[1] + pad[0] + pad[1]
 
     def stack_to_img(self, p_cts: list[PositionalCharTemplate], w: int, h: int) \
             -> np.ndarray: